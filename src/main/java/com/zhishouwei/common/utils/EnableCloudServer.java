ae1ddbbca4b5e81bd92c0175b6e339372199f8da
糊涂 <hutuygah@163.com>
2020-11-06 09:45:44 +0800

    导入Resources配置文件

--- src/main/java/com/zhishouwei/common/utils/EnableCloudServer.java
+++ src/main/java/com/zhishouwei/common/utils/EnableCloudServer.java
@@ -1,7 +1,10 @@
 package com.zhishouwei.common.utils;
 
+import com.zhishouwei.common.config.MybatisPlusConfig;
+import com.zhishouwei.common.config.OAuthResourceConfig;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Import;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
@@ -9,6 +12,7 @@ import java.lang.annotation.*;
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
+@Import({OAuthResourceConfig.class, MybatisPlusConfig.class})
 @EnableResourceServer
 @EnableFeignClients
 @SpringBootApplication
